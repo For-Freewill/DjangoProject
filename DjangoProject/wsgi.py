38d28d5ebe39f5ce42d1277f7ffba71ff77edd50
wulei <lei.wu06@ele.me>
2019-01-30 20:44:58 +0800

    add new

--- DjangoProject/wsgi.py
+++ DjangoProject/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for DjangoProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see2e
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
